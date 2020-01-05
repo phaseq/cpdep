8c672b9bc839bfe59013ef237239f210c03e3345
Fabian Beckmann <fb@phaseq.de>
2020-01-05 22:42:43 +0100

    cleanup

--- src/main.rs
+++ src/main.rs
@@ -14,10 +14,6 @@ struct Opt {
     #[structopt(long)]
     warn_missing: bool,
 
-    /// warn about malformed includes
-    //#[structopt(long)]
-    //warn_malformed: bool,
-
     /// show files for incoming dependencies
     #[structopt(long)]
     show_incoming: bool,
@@ -27,37 +23,18 @@ struct Opt {
     show_outgoing: bool,
 }
 
-/*
-func main() {
-    project := read_files(*root_dir, flags)
-    project.assign_files_to_components()
-    project.generate_file_deps(flags)
-    project.print_components(flags)
-    //project.dbg_files()
-}*/
-
 fn main() -> io::Result<()> {
     let opt = Opt::from_args();
     let mut project = read_files(&opt.root)?;
     project.assign_files_to_components();
     project.generate_file_deps(&opt);
     project.print_components(&opt);
-
-    //println!("{:?}", project);
     Ok(())
 }
 
 type ComponentRef = usize;
 type FileRef = usize;
 
-/*type file struct {
-    path          string
-    include_paths []string
-
-    component      *component
-    incoming_links []*file
-    outgoing_links []*file
-}*/
 #[derive(Debug)]
 struct File {
     path: String,
@@ -68,53 +45,12 @@ struct File {
     outgoing_links: Vec<FileRef>,
 }
 
-/*func (f *file) print() {
-    fmt.Printf("%s\n", f.path)
-    fmt.Printf("  Component: %s\n", f.component.nice_name())
-
-    fmt.Println("  Includes:")
-    for _, include := range f.include_paths {
-        fmt.Printf("    %s\n", include)
-    }
-
-    fmt.Println("  Incoming:")
-    for _, fo := range f.incoming_links {
-        fmt.Printf("    %s\n", fo.path)
-    }
-
-    fmt.Println("  Outgoing:")
-    for _, fo := range f.outgoing_links {
-        fmt.Printf("    %s\n", fo.path)
-    }
-}*/
-/*impl File {
-    fn print(&self, project: &Project) {
-        println!("{}", self.path);
-        println!(
-            "  Component: {}",
-            project.component(self.component.unwrap()).path
-        );
-
-        // TODO
-    }
-}*/
-
-/*type component struct {
-    path  string
-    files []*file
-}*/
 #[derive(Debug)]
 struct Component {
     path: String,
     files: Vec<FileRef>,
 }
 
-/*func (c *component) nice_name() string {
-    if c.path == "" {
-        return "."
-    }
-    return c.path
-}*/
 impl Component {
     fn nice_name(&self) -> &str {
         if self.path.is_empty() {
@@ -124,29 +60,11 @@ impl Component {
     }
 }
 
-/*type dependency struct {
-    component *component
-    edges     []edge
-}*/
-/*struct Dependency {
-    component: ComponentRef,
-    edges: Vec<Edge>,
-}*/
-
-/*type edge struct {
-    from *file
-    to   *file
-}*/
 struct Edge {
     from: FileRef,
     to: FileRef,
 }
 
-/*type project struct {
-    root       string
-    files      []file
-    components []component
-}*/
 #[derive(Debug)]
 struct Project {
     root: String,
@@ -163,28 +81,10 @@ impl Project {
         return &self.components[r];
     }
 
-    /*func (p *project) rel_path(path string) string {
-        rel_path := strings.TrimPrefix(strings.TrimPrefix(path, p.root), "/")
-        return rel_path
-    }*/
     fn rel_path<'a>(&self, path: &'a str) -> &'a str {
         return path.trim_start_matches(&self.root).trim_start_matches('/');
     }
 
-    /*func (p *project) print_components(flags log_flags) {
-        for _, c := range p.components {
-            should_print := len(flags.components) == 0
-            for _, name := range flags.components {
-                if name == c.nice_name() {
-                    should_print = true
-                    break
-                }
-            }
-            if should_print {
-                c.print(flags)
-            }
-        }
-    }*/
     fn print_components(&self, options: &Opt) {
         for (c_ref, c) in self.components.iter().enumerate() {
             let c_name = c.nice_name();
@@ -194,44 +94,12 @@ impl Project {
         }
     }
 
-    /*func (c *component) print(flags log_flags) {
-        fmt.Printf("%s (%d)\n", c.nice_name(), len(c.files))
-
-        in, out := c.linked_components()
-        sort.Slice(in, func(i, j int) bool {
-            return in[i].component.path < in[j].component.path
-        })
-        sort.Slice(out, func(i, j int) bool {
-            return out[i].component.path < out[j].component.path
-        })
-
-        fmt.Println("  Incoming:")
-        for _, dep := range in {
-            fmt.Printf("    %s\n", dep.component.nice_name())
-            if flags.show_incoming {
-                for _, e := range dep.edges {
-                    fmt.Printf("      %s -> %s\n", e.from.path, e.to.path)
-                }
-            }
-        }
-
-        fmt.Println("  Outgoing:")
-        for _, dep := range out {
-            fmt.Printf("    %s\n", dep.component.nice_name())
-            if flags.show_outgoing {
-                for _, e := range dep.edges {
-                    fmt.Printf("      %s -> %s\n", e.from.path, e.to.path)
-                }
-            }
-        }
-
-        fmt.Println("  Files:")
-        for _, f := range c.files {
-            fmt.Printf("   %s\n", f.path)
-        }
-    }*/
     fn print_component(&self, c: ComponentRef, options: &Opt) {
-        println!("{} ({})", self.component(c).nice_name(), self.files.len());
+        println!(
+            "{} ({})",
+            self.component(c).nice_name(),
+            self.component(c).files.len()
+        );
 
         let (dep_in, dep_out) = self.linked_components(c);
         println!("  Incoming:");
@@ -261,51 +129,8 @@ impl Project {
                 }
             }
         }
-
-        /*println!("  Files:");
-        for f in self.component(c).files.iter() {
-            println!("    {}", self.file(*f).path);
-        }*/
     }
 
-    /*func (p *project) print_files() {
-        for _, f := range p.files {
-            f.print()
-        }
-    }*/
-    /*fn print_files(&self) {
-        for f in self.files.iter() {
-            f.print(&self);
-        }
-    }*/
-
-    /*func (c *component) linked_components() ([]dependency, []dependency) {
-        incoming := make(map[*component][]edge)
-        outgoing := make(map[*component][]edge)
-        for f_index, f := range c.files {
-            for _, in := range f.incoming_links {
-                if in.component.path != c.path {
-                    incoming[in.component] = append(
-                        incoming[in.component], edge{from: in, to: c.files[f_index]})
-                }
-            }
-            for _, out := range f.outgoing_links {
-                if out.component.path != c.path {
-                    outgoing[out.component] = append(
-                        outgoing[out.component], edge{from: c.files[f_index], to: out})
-                }
-            }
-        }
-        in := make([]dependency, 0, len(incoming))
-        for k := range incoming {
-            in = append(in, dependency{component: k, edges: incoming[k]})
-        }
-        out := make([]dependency, 0, len(outgoing))
-        for k := range outgoing {
-            out = append(out, dependency{component: k, edges: outgoing[k]})
-        }
-        return in, out
-    }*/
     fn linked_components(
         &self,
         c: ComponentRef,
@@ -343,34 +168,6 @@ impl Project {
         (incoming, outgoing)
     }
 
-    /*func (p *project) assign_files_to_components() {
-        for i_file, file := range p.files {
-            // Iterate over prefixes of file path, to find the most specific component.
-            // Assign to the most specific component.
-            // Example: file path: "a/b/header.hpp"
-            // candidate 1: a/b
-            // candidate 2: a
-            // candidate 3: ''
-            idx := 0
-            path := file.path
-            for idx != -1 {
-                idx = strings.LastIndex(path, "/")
-                if idx != -1 {
-                    path = path[:idx]
-                } else {
-                    path = ""
-                }
-                for i_c, c := range p.components {
-                    if c.path == path {
-                        p.components[i_c].files = append(c.files, &p.files[i_file])
-                        p.files[i_file].component = &p.components[i_c]
-                        idx = -1
-                        break
-                    }
-                }
-            }
-        }
-    }*/
     fn assign_files_to_components(&mut self) {
         for (i_file, file) in self.files.iter_mut().enumerate() {
             // Iterate over prefixes of file path, to find the most specific component.
@@ -408,49 +205,6 @@ impl Project {
             }
         }
     }
-
-    /*func (p *project) generate_file_deps(flags log_flags) {
-        // map from possible include paths to corresponding files
-        // for example: "a/b/header.h" could be included as "header.h", "b/header.h", and "a/b/header.h"
-        // assumption here: normalized paths with unix slashes
-        path_to_files := make(map[string][]*file)
-        for i_file, file := range p.files {
-            path := file.path
-            path_to_files[path] = append(path_to_files[path], &p.files[i_file])
-            for idx := strings.Index(path, "/"); idx != -1; idx = strings.Index(path, "/") {
-                path = path[idx+1:]
-                path_to_files[path] = append(path_to_files[path], &p.files[i_file])
-            }
-        }
-
-        for i_file, file := range p.files {
-            for _, include := range file.include_paths {
-                deps, present := path_to_files[include]
-                if present {
-                    // If a file can be included from the current solution, assume that it is.
-                    // This avoids adding dependencies to headers with name clashes (like StdAfx.h).
-                    is_present_in_this_component := false
-                    for _, dep := range deps {
-                        if dep.component == file.component {
-                            is_present_in_this_component = true
-                            break
-                        }
-                    }
-                    if !is_present_in_this_component {
-                        for _, dep := range deps {
-                            p.files[i_file].outgoing_links =
-                                append(p.files[i_file].outgoing_links, dep)
-
-                            dep.incoming_links =
-                                append(dep.incoming_links, &p.files[i_file])
-                        }
-                    }
-                } else if flags.warn_missing {
-                    fmt.Printf("Include not found in %s: %s\n", file.path, include)
-                }
-            }
-        }
-    }*/
 
     fn generate_file_deps(&mut self, options: &Opt) {
         // map from possible include paths to corresponding files
@@ -497,44 +251,6 @@ impl Project {
     }
 }
 
-/*func read_files(root_path string, flags log_flags) project {
-    source_suffixes := []string{".cpp", ".hpp", ".c", ".h"}
-    ignore_patterns := []string{".svn", "dev/tools"}
-
-    root_path = strings.TrimSuffix(root_path, "/")
-
-    project := project{root: root_path}
-
-    err := filepath.Walk(project.root, func(path string, info os.FileInfo, err error) error {
-        if err != nil {
-            fmt.Printf("prevent panic by handling failure accessing a path %q: %v\n", path, err)
-            return err
-        }
-        for _, pattern := range ignore_patterns {
-            if strings.Contains(path, pattern) {
-                fmt.Printf("skipping: %s\n", path)
-                return filepath.SkipDir
-            }
-        }
-        if info.Name() == "CMakeLists.txt" {
-            component_path := project.rel_path(strings.TrimSuffix(path, "/CMakeLists.txt"))
-            project.components = append(project.components, component{path: component_path})
-        }
-        for _, suffix := range source_suffixes {
-            if strings.HasSuffix(path, suffix) {
-                include_paths := extract_includes(path, flags)
-                new_file := file{path: project.rel_path(path), include_paths: include_paths}
-                project.files = append(project.files, new_file)
-            }
-        }
-        return nil
-    })
-    if err != nil {
-        fmt.Printf("error walking the path %q: %v\n", project.root, err)
-        panic(err)
-    }
-    return project
-}*/
 fn read_files(root_path: &str) -> io::Result<Project> {
     let source_suffixes = [".cpp", ".hpp", ".c", ".h"];
     //let ignore_patterns = [".svn", "dev/tools"];
@@ -577,77 +293,14 @@ fn read_files(root_path: &str) -> io::Result<Project> {
     Ok(project)
 }
 
-/*func extract_includes(path string, flags log_flags) []string {
-    fh, err := os.Open(path)
-    check(err)
-    defer fh.Close()
-
-    var results []string
-
-    r := bufio.NewScanner(bufio.NewReader(fh))
-    for r.Scan() {
-        if strings.HasPrefix(r.Text(), "#include") {
-            line := r.Text()
-            iStart := strings.IndexAny(line, "\"<")
-            iEnd := strings.LastIndexAny(line, "\">")
-            if iStart == -1 || iEnd == -1 || iStart >= iEnd {
-                if flags.warn_malformed {
-                    fmt.Printf("malformed #include in %s: %s\n", path, line)
-                }
-                continue
-            }
-            include_path := line[(iStart + 1):iEnd]
-            if strings.Contains(include_path, "\\") || strings.Contains(include_path, "..") {
-                if flags.warn_malformed {
-                    fmt.Printf("malformed #include in %s: %s\n", path, include_path)
-                }
-                continue
-            }
-            results = append(results, include_path)
-        }
-    }
-
-    return results
-}*/
 fn extract_includes(path: &Path) -> io::Result<Vec<String>> {
     let mut results = Vec::new();
     let mut f = std::fs::File::open(path)?;
     let mut c = Vec::new();
     f.read_to_end(&mut c)?;
-    //let mut line = String::new();
-    //while r.read_line(&mut line)? != 0 {
-    /*for line in r.lines() {
-        let line = line?;
-        if !line.starts_with("#include") {
-            continue; // TODO: handle "#   include  <header.h> // <>?"
-        }
-        let i_start = line.find(|c| c == '"' || c == '<');
-        let i_end = line.rfind(|c| c == '"' || c == '>');
-        if i_start.is_none() || i_end.is_none() || i_start.unwrap() >= i_end.unwrap() {
-            if options.warn_malformed {
-                println!("malformed #include in {:?}: {}", path, line);
-            }
-            continue;
-        }
-        let include_path = &line[(i_start.unwrap() + 1)..i_end.unwrap()];
-        if include_path.contains('\\') || include_path.contains("..") {
-            if options.warn_malformed {
-                println!("malformed #include in {:?}: {}", path, line);
-            }
-            continue;
-        }
-        results.push(include_path.into());
-    }
-    Ok(results)*/
     let re = regex::bytes::Regex::new("#include [<\"]([^>\"]+)[>\"]").unwrap();
     for cap in re.captures_iter(&c) {
         results.push(String::from_utf8_lossy(&cap[1]).into());
     }
     Ok(results)
 }
-
-/*func check(e error) {
-    if e != nil {
-        panic(e)
-    }
-}*/
